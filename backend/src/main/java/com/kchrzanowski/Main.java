5959834702db14a2f47b2c1d1dde24b8171049b2
Konrad <chrzanekk@gmail.com>
2024-09-17 16:51:07 +0200

    clear unused imports

--- backend/src/main/java/com/kchrzanowski/Main.java
+++ backend/src/main/java/com/kchrzanowski/Main.java
@@ -1,19 +1,13 @@
 package com.kchrzanowski;
 
-import com.github.javafaker.Faker;
-import com.kchrzanowski.customer.Customer;
-import com.kchrzanowski.customer.CustomerRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-
     }
 
 //    @Bean
