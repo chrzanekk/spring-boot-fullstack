8469d60de47a90e6ff761bb6edab0d8506e4382c
Konrad <chrzanekk@gmail.com>
2024-10-05 16:07:40 +0200

    exposed test controller for get method

--- backend/src/main/java/com/kchrzanowski/security/SecurityFilterChainConfig.java
+++ backend/src/main/java/com/kchrzanowski/security/SecurityFilterChainConfig.java
@@ -36,6 +36,8 @@ public class SecurityFilterChainConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.POST, "/api/v1/customers", "/api/v1/auth/login")
                         .permitAll()
+                        .requestMatchers(HttpMethod.GET, "/ping")
+                        .permitAll()
                         .anyRequest()
                         .authenticated())
                 .sessionManagement(httpSecuritySessionManagementConfigurer ->
