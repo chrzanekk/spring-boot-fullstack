c8c449ba3a713c01b76aef669467ae3d1dac7cc5
Konrad <chrzanekk@gmail.com>
2024-10-14 20:25:30 +0200

    testing deploy to vps

--- frontend/react/src/services/Client.js
+++ frontend/react/src/services/Client.js
@@ -13,7 +13,7 @@ const getAuthConfig = () => ({
 
 export const getCustomers = async () => {
     try {
-        return await api.get(`${import.meta.env.VITE_API_BASE_URL}/api/v1/customers`,
+        return await api.get(`/api/v1/customers`,
             getAuthConfig()
         )
     } catch (e) {
@@ -25,7 +25,7 @@ export const saveCustomer = async (customer) => {
     try {
         console.log(`url to backend: ${import.meta.env.VITE_API_BASE_URL}`)
         return await api.post(
-            `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers`,
+            `/api/v1/customers`,
             customer
         )
     } catch (e) {
@@ -36,7 +36,7 @@ export const saveCustomer = async (customer) => {
 export const updateCustomer = async (id, update) => {
     try {
         return await api.put(
-            `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers/${id}`,
+            `/api/v1/customers/${id}`,
             update,
             getAuthConfig()
         )
@@ -48,7 +48,7 @@ export const updateCustomer = async (id, update) => {
 export const deleteCustomer = async (id) => {
     try {
         return await api.delete(
-            `${import.meta.env.VITE_API_BASE_URL}/api/v1/customers/${id}`,
+            `/api/v1/customers/${id}`,
             getAuthConfig()
         )
     } catch (e) {
@@ -59,7 +59,7 @@ export const deleteCustomer = async (id) => {
 export const login = async (usernameAndPassword) => {
     try {
         return await api.post(
-            `${import.meta.env.VITE_API_BASE_URL}/api/v1/auth/login`,
+            `/api/v1/auth/login`,
             usernameAndPassword
         )
     } catch (e) {
